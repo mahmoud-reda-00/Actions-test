14c3bfd4c6be993272b612193016702efcce67f2
Mahmoud Saafan <mamoudreda8@gmail.com>
2026-07-22 18:46:30 +0300

    update packege.json and add test.sh script

---
+++ src/app.js
@@ -0,0 +1,11 @@
+function greet(name) {
+    
+    return `Hello, ${name}!`;
+    }
+
+     module.exports = greet; //Ensures that the greet function can be imported and used in other files
+
+    if (require.main === module) {
+
+    console.log(greet("World"));
+}
---
+++ src/test.sh
@@ -0,0 +1,21 @@
+#!bin/bash
+
+#src/test.sh
+
+EXPECTED="Hello, test!"
+
+OUTPUT=$(node -e "console.log(require('./src/app')('test'));")
+
+if [ "$OUTPUT" == "$EXPECTED" ]; then
+
+  echo "Test passed!"
+
+  exit 0
+else
+
+  echo "Test failed. Expected '$EXPECTED' but got '$OUTPUT'."
+
+  exit 1
+
+fi
+
